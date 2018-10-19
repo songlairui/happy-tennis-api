497ade0e529f02ad33cb442005d6412591e39106
songlairui <cnre@live.cn>
2018-10-20 00:09:44 +0800

    add token create

--- routes/hi.js
+++ routes/hi.js
@@ -1,3 +1,18 @@
+const JWT = require('jsonwebtoken')
+const Joi = require('joi')
+
+const generateJwt = ({ id }) => {
+  return JWT.sign(
+    {
+      id,
+      exp: Math.floor(new Date().getTime() / 1000) + 7 * 24 * 60 * 60
+    },
+    process.env.JWT_SECRET
+  )
+}
+
+const tags = ['api', 'tests']
+
 module.exports = [
   {
     _: ['/hi', 'GET'],
@@ -5,9 +20,26 @@ module.exports = [
       return 'hello'
     },
     config: {
-      tags: ['api', 'tests'],
+      tags,
       description: '测试 hello-world',
       auth: false
+    }
+  },
+  {
+    _: ['/jwt', 'POST'],
+    async handler(request) {
+      return generateJwt({ id: 1 })
+    },
+    config: {
+      tags,
+      auth: false,
+      validate: {
+        payload: {
+          pwd: Joi.string()
+            .required()
+            .description('身份标示')
+        }
+      }
     }
   }
 ]
