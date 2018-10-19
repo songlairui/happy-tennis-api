079374408c2b90138b3b2b40cb0b674917275469
songlairui <cnre@live.cn>
2018-10-19 10:31:27 +0800

    fullfill pagniation options

--- plugins/pagination.js
+++ plugins/pagination.js
@@ -1,16 +1,38 @@
 const hapiPagination = require('hapi-pagination')
-
 const options = {
-  query: {},
-  meta: {
-    name: 'meta'
+  query: {
+    page: {
+      name: 'the_page' // The page parameter will now be called the_page
+    },
+    limit: {
+      name: 'per_page', // The limit will now be called per_page
+      default: 10       // The default value will be 10
+    }
   },
-  results: {
-    name: 'results'
-  },
-  routes: {}
-}
-
+   meta: {
+      location: 'body', // The metadata will be put in the response body
+      name: 'metadata', // The meta object will be called metadata
+      count: {
+          active: true,
+          name: 'count'
+      },
+      pageCount: {
+          name: 'totalPages'
+      },
+      self: {
+          active: false // Will not generate the self link
+      },
+      first: {
+          active: false // Will not generate the first link
+      },
+      last: {
+          active: false // Will not generate the last link
+      }
+   },
+   routes: {
+       include: ['/users', '/accounts', '/persons', '/'],
+   }
+};
 module.exports = {
   plugin: hapiPagination,
   options
